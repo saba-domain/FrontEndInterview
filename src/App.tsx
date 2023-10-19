9303e41453a6635aa49db8094dec301f0efb72d9
Danny Cui <cui_chong@hotmail.com>
2023-10-19 11:44:22 +1100

    home page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>Domain RTA skill practice</h1>
       </header>
+      <section className="App-section">
+        <h2>Collapsible Menu</h2>
+        <h3>Requirements:</h3>
+        <p>
+          1. Create a collapsible component that allows the user to configure{" "}
+          <b>multi-level</b> side menu
+        </p>
+        <p>2. Design a configurable data structure</p>
+        <p>*3. Mind about the performance</p>
+      </section>
+      <section className="App-section">
+        <h2>Add your solution here:</h2>
+      </section>
     </div>
   );
 }
